f9163d4be4decb6467d3b90f3cce930cf3439d17
doraemon0627 <69860154+doraemon0627@users.noreply.github.com>
2021-03-22 13:48:34 +0900

    added comments

--- queue.c
+++ queue.c
@@ -36,22 +36,39 @@ bool IsEmpty(void)
 
 void EnQueue(QData data)
 {
-    if (IsFull())
+    if (IsFull()) //if queue is full
         puts("Enqueue error: queue is full");
-    else
+    else //if not
         CopyQData(&g_queue[++g_rear].data, data);
-}
+    /* Does postfix increacement and g_rear will be increase after CopyQData excuted 
+    because one new element is added to queue and g_rear will point new element which is added to queue
+    
+    calls CopyQData to copy data(new element) to queue
+    I can just do like : g_queue[++g_rear].data = data but it's less safer than assigning member of structer one-by-one
+    
+    ex:)
+    s1.a = s1.a
+    s1.d = s1.d
+    
+    is more safer than
+    
+    s1 = s2
+    
+    */
 
 QData DeQueue(void)
 {
-    QData dequeued_data = {0};
+    QData dequeued_data = {0}; 
 
-    if (IsEmpty())
+    if (IsEmpty()) // if queue is empty
         puts("Dequeue error: queue is emty");
-    else
+    else//if not
     {
         CopyQData(&dequeued_data, g_queue[++g_front].data);
+        /* copies dequeued data(first element in queue) to dqueued_data
+        after copyqdata is called, g_front will be increased and It will point first element not dequeued element(first element before dequeue)
+        after calling copyqdata.
     }
 
-    return dequeued_data;
+    return dequeued_data; //returns dequeued data(data of dequeued element) 
 }
